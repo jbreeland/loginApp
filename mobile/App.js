39dc62537b808974e71e9bdef81b5f263f3471f3
Jason Breeland <jbreeland@milend.com>
2024-06-10 22:43:39 -0400

    hoping to add mobile folder. Issue is with /screens/AuthScreen.jsorigin

---
+++ mobile/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { AuthScreen } from './screens';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <AuthScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
